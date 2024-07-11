ba018915d458133bd5e689be59d9ba29d9413f17
Sage1918 <1918aju@gmail.com>
2024-07-11 21:49:20 +0530

    nvim: Fix mistake where some telescope keybinds were left out

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,12 +38,6 @@ local on_attach = function(_, bufnr)
   nmap('<leader>lrn', vim.lsp.buf.rename, '[L]sp [R]e[n]ame')
   nmap('<leader>lca', vim.lsp.buf.code_action, '[L]sp [C]ode [A]ction')
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
   nmap('<leader>lf', vim.lsp.buf.format, '[L]sp [F]ormat')
 
   -- See `:help K` for why this keymap
--- nvim/lua/plugins/telescope-nvim/keymaps.lua
+++ nvim/lua/plugins/telescope-nvim/keymaps.lua
@@ -27,3 +27,11 @@ vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by 
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymap' })
+
+vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
+vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
+vim.keymap.set('n', '<leader>ltd', require('telescope.builtin').lsp_type_definitions, { desc = 'Type [D]efinition' })
+vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = '[D]ocument [S]ymbols' })
+vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+  { desc = '[W]orkspace [S]ymbols' })
