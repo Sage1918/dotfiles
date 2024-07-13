099f9ba2ff6cdad00f625727f69bcfad33dcbb2d
Sage1918 <1918aju@gmail.com>
2024-07-13 21:41:17 +0530

    nvim: Updated the config for nvim-cmp

--- nvim/lua/plugins/nvim-cmp/init.lua
+++ nvim/lua/plugins/nvim-cmp/init.lua
@@ -1,8 +1,12 @@
 M = {
   'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
   dependencies = {
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
+
+    -- nvim lua source
+    'hrsh7th/cmp-nvim-lua',
 
     -- Adds LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
@@ -11,6 +15,7 @@ M = {
     -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
   },
+  config = require'plugins.nvim-cmp.config'
 }
 
 return M
