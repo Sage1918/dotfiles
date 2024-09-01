6ade9143724b0512ae364984d3b0bb710fec2cdd
Sage1918 <1918aju@gmail.com>
2024-09-01 17:49:48 +0530

    nvim: Add keybind to quickly traverse quickfix list
    Changed lsp signature help keymap to '<leader>k'

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,2 +1,5 @@
 vim.keymap.set('n', 'j', "v:count == 0? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set('n', 'k', "v:count == 0? 'gk' : 'k'", { expr = true, silent = true })
+
+vim.keymap.set('n', '<C-j>', ':cnext<CR>', { silent = true })
+vim.keymap.set('n', '<C-k>', ':cprev<CR>', { silent = true })
