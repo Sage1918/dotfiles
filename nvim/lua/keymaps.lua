d9aaa3b233639f8e223d3526861993f3ca0a6505
Sage1918 <1918aju@gmail.com>
2025-07-13 19:56:44 +0530

    nvim: Add keymap to quickly open new tab.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', 'k', "v:count == 0? 'gk' : 'k'", { expr = true, silent = tru
 
 vim.keymap.set('n', '<C-j>', ':cnext<CR>', { silent = true })
 vim.keymap.set('n', '<C-k>', ':cprev<CR>', { silent = true })
+
+vim.keymap.set('n', '<leader>nt', ':tabnew<CR>', { silent = true })
